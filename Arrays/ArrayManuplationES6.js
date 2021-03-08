155e4bff62c028c22eeebff08c8b80c74addb216
srashtiguptagit <srashtigupta1708@gmail.com>
2021-03-08 19:46:47 +0530

    objects

--- Arrays/ArrayManuplationES6.js
+++ Arrays/ArrayManuplationES6.js
@@ -37,7 +37,7 @@ console.log(newArr);
 
 //2
 
-// 4. Supoose we want to find the highest element in an array
+// 4. Suppose we want to find the highest element in an array
 
 var arr = [-1, 2, -3, 4];
 
@@ -50,5 +50,77 @@ var newArr = arr.reduce((highest, inital)=> {
   }
 });
 
-console.log(newArr); //4
+console.log(newArr); // 4
 
+//5. Find method (returns the first matched element)
+
+var arr = [-1, 2, -3, 4, 5, 6];
+
+var newArr = arr.find((val) => {
+  return val > 3
+  
+})
+
+console.log(newArr);  // 4
+
+//===============================================================
+
+//join Method
+
+var arr= ['banana', 'grapes', 'oranges'];
+
+var newArr = arr.join('+');
+
+console.log(newArr);
+console.log(typeof(newArr));
+
+// "banana+grapes+oranges"
+// "string"
+
+// Push method=> adds the elements to end of the array
+
+var arr= ['banana', 'grapes', 'oranges'];
+arr.push('mango');
+for(var key in arr) {
+  console.log(arr[key]);
+}
+
+// "banana"
+// "grapes"
+// "oranges"
+// "mango"
+
+// Unshift => adds  the element to start of the array
+
+var arr= ['banana', 'grapes', 'oranges'];
+arr.unshift('apple');
+
+console.log(arr);
+
+//Pop method removes last element from array
+
+var arr= ['banana', 'grapes', 'oranges'];
+var x = arr.pop();
+
+console.log(x); // oranges
+
+// Removes the first element from array
+var arr= ['banana', 'grapes', 'oranges'];
+var x = arr.shift();
+
+console.log(x);
+
+// Splice method
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+// ["Banana", "Orange", "Apple", "Lemon", "Kiwi", "Mango"]
+
+// Slice method
+
+var arr= ['banana', 'grapes', 'oranges'];
+var x= arr.slice(1);
+console.log(x);
+
+//["grapes", "oranges"]
