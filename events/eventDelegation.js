bf90bf5e16dc5a85efdbd08553676ae3a715057d
srashtiguptagit <srashtigupta1708@gmail.com>
2021-03-10 18:00:17 +0530

    objects

---
+++ events/eventDelegation.js
@@ -0,0 +1,27 @@
+// With event bubbling and capturing The idea is that if we have a lot of elements handled in a similar way, then instead of 
+// assigning a handler to each of them – we put a single handler on their common ancestor. 
+
+<html>
+<table onclick="abc()">
+  <tr>
+    <th colspan="3"><em>Bagua</em> Chart: Direction, Element, Color, Meaning</th>
+  </tr>
+  <tr>
+    <td class="nw"><strong>Northwest</strong><br>Metal<br>Silver<br>Elders</td>
+    <td class="n">...</td>
+    <td class="ne">...</td>
+  </tr>
+  <tr>...2 more lines of this kind...</tr>
+  <tr>...2 more lines of this kind...</tr>
+</table>
+
+table.onclick = function(event) {
+  let target = event.target; // where was the click?
+
+  if (target.tagName != 'TD') return; // not on TD? Then we're not interested
+
+  highlight(target); // highlight it
+};
+
+
+</html>
