8fadd6d015d20d52f5b731e4e5185c561eb5e4fa
srashtiguptagit <srashtigupta1708@gmail.com>
2021-03-27 16:26:35 +0530

    hackerrank problems

---
+++ HTML5_APIs/iframes.js
@@ -0,0 +1,17 @@
+// HTML Iframe Syntax
+// The HTML <iframe> tag specifies an inline frame.
+// An inline frame is used to embed another document within the current HTML document.
+
+<iframe src="demo_iframe.htm" height="200" width="300" title="Iframe Example">  // url of the document to be displayed 
+
+</iframe>
+
+
+// Iframe - Target for a Link
+// An iframe can be used as the target frame for a link.
+
+// The target attribute of the link must refer to the name attribute of the iframe:
+
+{/* <iframe src="demo_iframe.htm" name="iframe_a" title="Iframe Example"></iframe>
+
+<p><a href="https://www.w3schools.com" target="iframe_a">W3Schools.com</a></p> */}
